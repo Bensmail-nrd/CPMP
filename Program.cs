e8291e6047a44fb46c023890721052696d1c3b4c
bensmail noureddine <bensmail.nrd@gmail.com>
2025-04-22 14:48:58 +0100

    Add a phrase

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
           options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 var app = builder.Build();
+// Nothing to add
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
